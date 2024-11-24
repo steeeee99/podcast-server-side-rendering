9dfb95c2094ec3dcf4c45ac36770714ffbd93d68
stemasuelli999 <118314835+steeeee99@users.noreply.github.com>
2024-11-24 22:21:56 +0100

    deleted v1 files

--- public/javascripts/logout-button.js
+++
@@ -1,7 +0,0 @@
-const button = document.querySelector('#logout-button');
-
-button.addEventListener('click',()=>{
-    fetch(`/session/current`,{method:'DELETE'}).then(()=>{
-        window.location = '/';
-    })
-})
